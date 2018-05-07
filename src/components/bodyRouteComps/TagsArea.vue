742f0212a7e2d97253c2fe5ab60426245afa8804
SakilaWAW <pccp0909@163.com>
2018-05-07 18:48:10 +0800

    完成tag标签页

--- src/components/bodyRouteComps/TagsArea.vue
+++ src/components/bodyRouteComps/TagsArea.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="tags-area">
-    tags
+    <div class="ta-title">目前共计{{ tagData.length }}个标签</div>
+    <div class="tag-bar">
+      <auto-adapt-text v-for="(data,idx) in tagData" :key="idx"
+                       :size="data.count" :text="data.tag"></auto-adapt-text>
+    </div>
   </div>
 </template>
 
 <script>
+  import AutoAdaptText from '../commons/AutoAdaptText';
+
   export default {
     name: 'tags-area',
+    created() {
+      this.fetchTagData();
+    },
+    components: {
+      AutoAdaptText,
+    },
+    data() {
+      return {
+        tagData: [],
+      };
+    },
+    methods: {
+      fetchTagData() {
+        this.$axios.get('http://127.0.0.1:3000/stats/tag')
+          .then(res=> {
+            this.tagData = res.data;
+          }).catch(err=>{
+            console.log(`fetchTagData出问题了${err}`);
+        });
+      },
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-
+  .ta-title {
+    font-size: 17px;
+    text-align: center;
+    margin-bottom: 25px;
+  }
 </style>
