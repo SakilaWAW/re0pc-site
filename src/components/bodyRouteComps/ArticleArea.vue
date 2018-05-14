d9f2513cb4d89a8af5c3807d7f5fba3e8f6aac18
SakilaWAW <pccp0909@163.com>
2018-05-14 20:00:41 +0800

    使用highlight.js为markdown中的code着色

--- src/components/bodyRouteComps/ArticleArea.vue
+++ src/components/bodyRouteComps/ArticleArea.vue
@@ -9,6 +9,7 @@
 <script>
   import ArticleSubInfo from '../commons/ArticleSubInfo';
   import marked from 'marked';
+  import 'highlight.js/styles/github-gist.css';
 
   export default {
     name: 'article-area',
@@ -20,10 +21,35 @@
         this.fetchData();
       }
       this.updateCount();
+      let hljs = require('highlight.js');
+
+      let diyRender = new marked.Renderer();
+      diyRender.heading = (text, level) =>{
+        return `<h${level} id="${text}">${text}</h${level}>`
+      };
+
+      marked.setOptions({
+        renderer: diyRender,
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: false,
+        smartLists: true,
+        smartypants: false,
+        highlight: function (code, lang) {
+          if (lang && hljs.getLanguage(lang)) {
+            return hljs.highlight(lang, code, true).value;
+          } else {
+            return hljs.highlightAuto(code).value;
+          }
+        }
+      });
     },
     data() {
       return {
         article: this.$store.state.cur_article,
+        catalog: '',
       };
     },
     methods: {
