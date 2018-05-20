06b54c21868d5231582efda291d202e7eccc5630
SakilaWAW <pccp0909@163.com>
2018-05-20 17:21:28 +0800

    添加无目录时的侧边栏效果

--- src/components/commons/CatalogPage.vue
+++ src/components/commons/CatalogPage.vue
@@ -1,8 +1,9 @@
 <template>
   <ul class="catalog-page">
-    <li v-for="(cata,idx) in cataData" :key="idx">
+    <li class="cata-item" v-for="(cata,idx) in cataData" :key="idx">
       <a class="cata-href" :href="`#${cata.text}`" :style="{'padding-left': `${cata.step*20}px`}">{{ cata.text }}</a>
     </li>
+    <li class="nothing-to-show" v-if="cataData.length===0">此文章无目录</li>
   </ul>
 </template>
 
@@ -21,8 +22,10 @@
   @import '../../css/global';
   .catalog-page {
     list-style-type: none;
-    text-align: left;
+  }
+  .cata-item {
     padding-left: 30px;
+    text-align: left;
   }
   .cata-href {
     color: $high-light-text-grey;
@@ -32,5 +35,10 @@
   }
   .cata-href:hover {
     color: white;
+  }
+  .nothing-to-show {
+    color: $high-light-text-grey;
+    font-size: 16px;
+    font-weight: bold;
   }
 </style>
